fa3a30c8c0df95e44abf5c0741cbdca9e2d6bff9
chen24102801 <chen241028@gmail.com>
2026-06-21 13:56:45 +0800

    把 <iframe> 換成地圖容器

--- src/views/medical/ClinicSearch.vue
+++ src/views/medical/ClinicSearch.vue
@@ -195,14 +195,7 @@
 
       <!-- 【右側地圖區】 -->
       <div class="right-map-panel pawcare-card">
-        <iframe
-          class="embedded-google-map"
-          src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m124.536!2d121.543!3d25.033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1z5YuV54mp6Yar6Zmi!5e0!3m2!1szh-TW!2stw!4v1710000000000!5m2!1szh-TW!2stw"
-          frameborder="0"
-          scrolling="no"
-          marginheight="0"
-          marginwidth="0"
-        ></iframe>
+        <div id="clinicMap" class="embedded-google-map"></div>
       </div>
     </div>
   </div>
